7fd95aad0e9da9ec5d6cdc96a36c80b8c974dfd6
Artur <raczek.artur@wp.pl>
2018-08-01 18:20:46 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args){
+
+
+        String tekst = "Witaj w świecie";
+        String tekst_2 = "Witaj w świecie 2";
+        String tekst_v3 = "Witaj w świecie 3";
+
+        System.out.println(tekst);
+
+    }
+
+
+
+}
